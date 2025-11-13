1521256ef197f2374fd136658e22ae525c1c0135
OikoumE <oikoume@gmail.com>
2025-11-13 17:39:10 +0100

    disable treesj

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -8,7 +8,7 @@ return {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
     explorer = { enabled = true },
-    indent = { enabled = true },
+    indent = { enabled = false },
     input = { enabled = true },
     notifier = {
       enabled = true,
@@ -230,8 +230,7 @@ return {
       end,
       desc = 'Registers',
     },
-    -- {
-    --   '<leader>s/',
+    -- { '<leader>s/',
     --   function()
     --     Snacks.picker.search_history()
     --   end,
@@ -272,13 +271,13 @@ return {
       end,
       desc = 'Diagnostics',
     },
-    {
-      '<leader>sD',
-      function()
-        Snacks.picker.diagnostics_buffer()
-      end,
-      desc = 'Buffer Diagnostics',
-    },
+    -- {
+    --   '<leader>sD',
+    --   function()
+    --     Snacks.picker.diagnostics_buffer()
+    --   end,
+    --   desc = 'Buffer Diagnostics',
+    -- },
     -- {
     --   '<leader>sh',
     --   function()
@@ -335,13 +334,13 @@ return {
     --   end,
     --   desc = 'Man Pages',
     -- },
-    {
-      '<leader>sp',
-      function()
-        Snacks.picker.lazy()
-      end,
-      desc = 'Search for Plugin Spec',
-    },
+    -- {
+    --   '<leader>sp',
+    --   function()
+    --     Snacks.picker.lazy()
+    --   end,
+    --   desc = 'Search for Plugin Spec',
+    -- },
     -- {
     --   '<leader>sq',
     --   function()
@@ -363,21 +362,21 @@ return {
     --   end,
     --   desc = 'Undo History',
     -- },
-    {
-      '<leader>uC',
-      function()
-        Snacks.picker.colorschemes()
-      end,
-      desc = 'Colorschemes',
-    },
-    -- LSP
     -- {
-    --   'gd',
+    --   '<leader>uC',
     --   function()
-    --     Snacks.picker.lsp_definitions()
+    --     Snacks.picker.colorschemes()
     --   end,
-    --   desc = 'Goto Definition',
+    --   desc = 'Colorschemes',
     -- },
+    -- LSP
+    {
+      'gd',
+      function()
+        Snacks.picker.lsp_definitions()
+      end,
+      desc = 'Goto Definition',
+    },
     -- {
     --   'gD',
     --   function()
@@ -436,34 +435,34 @@ return {
     --   desc = 'LSP Workspace Symbols',
     -- },
     -- Other
-    {
-      '<leader>z',
-      function()
-        Snacks.zen()
-      end,
-      desc = 'Toggle Zen Mode',
-    },
-    {
-      '<leader>Z',
-      function()
-        Snacks.zen.zoom()
-      end,
-      desc = 'Toggle Zoom',
-    },
-    {
-      '<leader>.',
-      function()
-        Snacks.scratch()
-      end,
-      desc = 'Toggle Scratch Buffer',
-    },
-    {
-      '<leader>S',
-      function()
-        Snacks.scratch.select()
-      end,
-      desc = 'Select Scratch Buffer',
-    },
+    -- {
+    --   '<leader>z',
+    --   function()
+    --     Snacks.zen()
+    --   end,
+    --   desc = 'Toggle Zen Mode',
+    -- },
+    -- {
+    --   '<leader>Z',
+    --   function()
+    --     Snacks.zen.zoom()
+    --   end,
+    --   desc = 'Toggle Zoom',
+    -- },
+    -- {
+    --   '<leader>.',
+    --   function()
+    --     Snacks.scratch()
+    --   end,
+    --   desc = 'Toggle Scratch Buffer',
+    -- },
+    -- {
+    --   '<leader>S',
+    --   function()
+    --     Snacks.scratch.select()
+    --   end,
+    --   desc = 'Select Scratch Buffer',
+    -- },
     {
       '<leader>n',
       function()
@@ -471,20 +470,20 @@ return {
       end,
       desc = 'Notification History',
     },
-    {
-      '<leader>bd',
-      function()
-        Snacks.bufdelete()
-      end,
-      desc = 'Delete Buffer',
-    },
-    {
-      '<leader>cR',
-      function()
-        Snacks.rename.rename_file()
-      end,
-      desc = 'Rename File',
-    },
+    -- {
+    --   '<leader>bd',
+    --   function()
+    --     Snacks.bufdelete()
+    --   end,
+    --   desc = 'Delete Buffer',
+    -- },
+    -- {
+    --   '<leader>cR',
+    --   function()
+    --     Snacks.rename.rename_file()
+    --   end,
+    --   desc = 'Rename File',
+    -- },
     {
       '<leader>gB',
       function()
@@ -514,13 +513,13 @@ return {
       end,
       desc = 'Toggle Terminal',
     },
-    {
-      '<c-_>',
-      function()
-        Snacks.terminal()
-      end,
-      desc = 'which_key_ignore',
-    },
+    -- {
+    --   '<c-_>',
+    --   function()
+    --     Snacks.terminal()
+    --   end,
+    --   desc = 'which_key_ignore',
+    -- },
     {
       ']]',
       function()
@@ -537,24 +536,24 @@ return {
       desc = 'Prev Reference',
       mode = { 'n', 't' },
     },
-    {
-      '<leader>N',
-      desc = 'Neovim News',
-      function()
-        Snacks.win {
-          file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = 'yes',
-            statuscolumn = ' ',
-            conceallevel = 3,
-          },
-        }
-      end,
-    },
+    -- {
+    --   '<leader>N',
+    --   desc = 'Neovim News',
+    --   function()
+    --     Snacks.win {
+    --       file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
+    --       width = 0.6,
+    --       height = 0.6,
+    --       wo = {
+    --         spell = false,
+    --         wrap = false,
+    --         signcolumn = 'yes',
+    --         statuscolumn = ' ',
+    --         conceallevel = 3,
+    --       },
+    --     }
+    --   end,
+    -- },
   },
   init = function()
     vim.api.nvim_create_autocmd('User', {
@@ -580,15 +579,15 @@ return {
         -- Create some toggle mappings
         Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
         Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
-        Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>uL'
+        -- Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>uL'
         Snacks.toggle.diagnostics():map '<leader>ud'
-        Snacks.toggle.line_number():map '<leader>ul'
+        -- Snacks.toggle.line_number():map '<leader>ul'
         Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map '<leader>uc'
-        Snacks.toggle.treesitter():map '<leader>uT'
-        Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
-        Snacks.toggle.inlay_hints():map '<leader>uh'
-        Snacks.toggle.indent():map '<leader>ug'
-        Snacks.toggle.dim():map '<leader>uD'
+        -- Snacks.toggle.treesitter():map '<leader>uT'
+        -- Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
+        -- Snacks.toggle.inlay_hints():map '<leader>uh'
+        -- Snacks.toggle.indent():map '<leader>ug'
+        -- Snacks.toggle.dim():map '<leader>uD'
       end,
     })
   end,
